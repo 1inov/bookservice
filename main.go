abeec49ac87db1e4a60b7126261a74ba075530ab
1inov <109175607+1inov@users.noreply.github.com>
2022-07-13 04:41:37 +0300

    fix main
    go

---
+++ main.go
@@ -0,0 +1,75 @@
+package main
+
+import (
+	"bookservice/server"
+	"context"
+	"flag"
+	f "fmt"
+	"log"
+	"strconv"
+	"strings"
+
+	"google.golang.org/grpc"
+)
+
+func main() {
+
+	// получаем аргументы , их должно быть не от 2 до 3
+	flag.Parse()
+	if flag.NArg() < 2 {
+		log.Fatal("not enought arguments")
+	} else if flag.NArg() > 3 {
+		log.Fatal("too many arguments")
+	}
+
+	//Запись переменных из аргументов
+	fnc := flag.Arg(0)
+
+	like, err := strconv.ParseBool(flag.Arg(2))
+	name := flag.Arg(1)
+	if err != nil {
+		like = false
+	}
+
+	// подключение к серверу
+	conn, err := grpc.Dial(":8085", grpc.WithInsecure())
+	if err != nil {
+		log.Fatal(err)
+	}
+
+	c := server.NewBaseClient(conn)
+
+	res, err := Answer(c, like, fnc, name)
+	if err != nil {
+		log.Fatal(err)
+	}
+	println(res)
+
+}
+
+// Функция запроса серверу
+func Answer(c server.BaseClient, like bool, fnc, name string) (string, error) {
+	if strings.ToUpper(fnc) == "SEARCHBYAUTHOR" {
+		if flag.NArg() == 2 {
+			resp, err := c.SearchByAuthor(context.Background(), &server.RequestByAuthor{Like: like, Name: name})
+			if err != nil {
+				return f.Sprintf("error : %s", err), err
+			}
+			return f.Sprintf("Книги автора '%s' : %s", name, resp.GetAnswer()), nil
+		} else {
+			resp, err := c.SearchByAuthor(context.Background(), &server.RequestByAuthor{Like: like, Name: name})
+			if err != nil {
+				return f.Sprintf("error : %s", err), err
+			}
+			return f.Sprintf("Книги автора '%s' : %s", name, resp.GetAnswer()), nil
+		}
+	} else if strings.ToUpper(fnc) == "SEARCHBYBOOK" {
+		resp, err := c.SearchByBook(context.Background(), &server.RequestByBook{Like: like, Title: name})
+		if err != nil {
+			return f.Sprintf("error : %s", err), err
+		}
+		return f.Sprintf("Автор книги '%s' : %s", name, resp.GetAnswer()), nil
+	} else {
+		return f.Sprintf("error : %s", "wrong method"), f.Errorf("error : %s", "wrong method")
+	}
+}
